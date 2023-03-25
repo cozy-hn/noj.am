e016c41a484ae0635c36e90e6521b26582057d8d
jiko <jhk2721@gmail.com>
2023-03-25 16:11:21 +0900

    최대힙

---
+++ 11279.py
@@ -0,0 +1,12 @@
+import sys
+from heapq import heappush, heappop
+input=lambda:sys.stdin.readline().rstrip()
+heap=[]
+for _ in range(int(input())):
+    n=int(input())
+    if n!=0:
+        heappush(heap,(-n,n))
+    elif len(heap):
+        print(heappop(heap)[1])
+    else:
+        print(0)
