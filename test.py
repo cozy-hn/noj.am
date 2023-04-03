a10dfa51cdd60613493768ca1830a6ea31ae94d8
Jiheun Ko <jhk2721@gmail.com>
2023-04-03 22:36:08 +0900

    Fibonacci

--- test.py
+++
@@ -1,6 +0,0 @@
-import sys
-input=lambda: sys.stdin.readline().rstrip()
-
-for _ in range(int(input())):
-    money=int(input())
-    print(money//25,(money%25)//10,(money%25%10)//5,(money%25%10%5)//1)
