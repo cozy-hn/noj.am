7fae43c068f13e77e1b32d81d9f168dfd519e645
cozy-hn <jhk2721@gmail.com>
2023-05-03 00:12:45 +0900

    floyd

---
+++ library/floydwarshall.py
@@ -0,0 +1,6 @@
+def floywarshall(graph, n):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    return graph
