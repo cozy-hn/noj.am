38519f71fbf890876e30084e4afce7602256afb7
Jiheun Ko <jhk2721@gmail.com>
2023-08-02 17:07:22 +0900

    ㄱㅖ 단수

--- 1562.py
+++ 1562.py
@@ -1,12 +1,17 @@
 N=int(input())
 MOD=10**9
-dp=[0]+[1]*9
-tmp=[0]*10
-for _ in range(1,N):
-    for i in range(10):
-        tmp[i]=dp[i]
-    for i in range(1,9):
-        dp[i]=(tmp[i-1]+tmp[i+1])%MOD
-    dp[0]=tmp[1]
-    dp[9]=tmp[8]
-print(sum(dp)%MOD)
+dp=[[[0]*1024 for _ in range(10)] for _ in range(N+1)]
+for i in range(1,10):
+    dp[1][i][1<<i]=1
+
+for i in range(2,N+1):
+    for j in range(10):
+        for k in range(1024):
+            if j!=9 and j!=0:
+                dp[i][j][k|(1<<j)]+=dp[i-1][j-1][k]+dp[i-1][j+1][k]%MOD
+            elif j==0:
+                dp[i][j][k|(1<<j)]+=dp[i-1][j+1][k]%MOD
+            else:
+                dp[i][j][k|(1<<j)]+=dp[i-1][j-1][k]%MOD
+
+print(sum(dp[N][i][1023] for i in range(10))%MOD)
